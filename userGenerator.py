6743eff3d829aa68a29a452fb61ca9de79a0e9b6
Yang Encarnação <34986831+yangencarnacao@users.noreply.github.com>
2022-08-04 11:05:09 -0300

    Add files via upload
    upping code

---
+++ userGenerator.py
@@ -0,0 +1,6 @@
+# PROGRAMA QUE GERA NOME DOS USUARIOS DO SISTEMA
+
+fullNameUser = str(input('Digite o nome completo titular: ')).strip()
+nome = fullNameUser.split()
+print(nome)
+print("{}".format(nome[0]) + "." + "{}".format(nome[-1]))
